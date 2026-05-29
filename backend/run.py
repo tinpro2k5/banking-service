82ea1d18509b4dc5a14a053c647dcdcda1497c67
le trung tin <23120371@student.hcmus.edu.vn>
2026-05-29 11:32:40 +0700

    fix typo

--- backend/run.py
+++ backend/run.py
@@ -4,6 +4,6 @@ import uvicorn
 
 from app.core.settings import settings
 
-sss
+
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host=settings.api_host, port=settings.api_port, reload=False)
